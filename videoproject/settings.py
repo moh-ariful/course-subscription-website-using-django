350ebd85692de76e8b1a37470e7c024de7f4fa2f
Moh Ariful <nextkoding@gmail.com>
2022-11-11 08:21:12 +0700

    Regenerate superuser

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -114,6 +114,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 
+if DEBUG:
+    STRIPE_PUBLISHABLE_KEY = ''
+    STRIPE_SECRET_KEY = ''
+
+else:
+    # live keys
+    STRIPE_PUBLISHABLE_KEY = ''
+    STRIPE_SECRET_KEY = ''
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
